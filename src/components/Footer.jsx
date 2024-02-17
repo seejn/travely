eb6908db8e8999e1497784628e4804ebf6bd66b7
seejn <seejnmaharjan@gmail.com>
2024-02-17 12:21:36 +0545

    style: Improve whitespace management
    Spacing and Whitespaces

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-const Footer=()=>{
-   return <footer class="bg-gray-900 text-white py-4 mt-8">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"/>
-   <p class="text-center">
-     Contact us at: example@example.com | Phone: +123456789
-   </p>
- </footer>
-}
-export default Footer
+import React from "react";
+const Footer = () => {
+    return (
+        <footer className='bg-gray-900 text-white py-4 mt-8'>
+            <link
+                href='https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
+                rel='stylesheet'
+            />
+            <p className='text-center'>
+                Contact us at: example@example.com | Phone: +123456789
+            </p>
+        </footer>
+    );
+};
+export default Footer;
