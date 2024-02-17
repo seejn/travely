54aebb6ac2840ea5451c9958452baba6fb0c31cd
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-17 11:59:14 +0545

    header component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>Kismat</div>
+function Header(){
+  return(
+      <>
+  
+      <header class="bg-gray-900 text-white py-4">
+          <h1 class="text-center">Destination Travel Packages</h1>
+      </header>
+      <nav class="bg-gray-800 text-white py-2">
+        <a href="#" class="mx-2">Home</a>
+        <a href="#" class="mx-2">Packages</a>
+        <a href="#" class="mx-2">About Us</a>
+        <a href="#" class="mx-2">Contact</a>
+      </nav>
+      </>
   )
 }
 
-export default Header
+export default  Header; 
